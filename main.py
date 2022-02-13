5305da9a465f8418591d51e10875e81c8166c4ac
jamieyoung5 <jamie5young55@gmail.com>
2022-02-13 16:23:37 +0000

    Create main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import json
 import discord
+from discord.ext import tasks
+from discord.ext import commands
+
+#read config file
+with open('config/config.json', 'r') as file:
+    configFile = json.load(file)
+
+prefix = configFile["prefix"]
+token = configFile["token"]
+
+client = discord.Client()
+bot = commands.Bot(command_prefix=prefix)
+
