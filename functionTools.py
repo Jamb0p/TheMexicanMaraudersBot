116c0fef375bac82afd5dbe478c5f28a1499c56c
Jamb0p <99514248+Jamb0p@users.noreply.github.com>
2022-02-14 00:01:13 +0000

    added basic search functionality

---
+++ functionTools.py
@@ -0,0 +1,8 @@
+
+def SearchJSONList(list, target, targetField):
+    found = False
+    for i in list:
+        if i[targetField] == target:
+            found = True
+            return i
+    return None
