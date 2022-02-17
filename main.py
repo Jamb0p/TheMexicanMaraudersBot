ea79b7ec22172036d9eca417c7ae98555cc8bc90
jamieyoung5 <jamie5young55@gmail.com>
2022-02-17 15:37:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import discord
 from discord.ext import tasks
 from discord.ext import commands
+import pymysql.cursors
+
 
 #read config file
 with open('config/config.json', 'r') as file:
@@ -9,7 +11,20 @@ with open('config/config.json', 'r') as file:
 
 prefix = configFile["prefix"]
 token = configFile["token"]
+host = configFile["host"]
+user = configFile["user"]
+password = configFile["password"]
+database = configFile["database"]
+
 
 client = discord.Client()
 bot = commands.Bot(command_prefix=prefix)
+
+@bot.event
+async def on_ready():
+    connection = pymysql.connect(host=host,
+                                 user=user,
+                                 password=password,
+                                 database=database,
+                                 cursorclass=pymysql.cursors.DictCursor)
 
